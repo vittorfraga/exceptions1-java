0b0d713ea3872267527fdae0428737c886fbd20f
Carlos Fraga <vittorfraga@outlook.com>
2023-04-10 20:09:49 -0300

    2nd commit

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -40,9 +40,20 @@ public class Reservation {
         return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
     }
 
-    public void updateDates(Date checkIn, Date checkOut){
+    public String updateDates(Date checkIn, Date checkOut){
+
+        Date now = new Date();
+        if (checkIn.before(now) || checkOut.before(now)) {
+           return "Check-out date must be after check-in date";
+        }
+
+        if (!checkOut.after(checkIn)) {
+            return "Check-out date must be after check-in date";
+        }
+
         this.checkIn = checkIn;
         this.checkOut = checkOut;
+        return null;
     }
 
     @Override
@@ -56,7 +67,7 @@ public class Reservation {
             + ", "
             + duration()
             + " nights";
-    };
+    }
 
 
 }
